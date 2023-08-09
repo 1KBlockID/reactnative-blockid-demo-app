2e1173cca18a4af3d7f10ca882f494234e0c94c5
kajal verma <kajal.verma@1kosmos.com>
2023-08-09 16:38:17 +0530

    changes for image name

--- demoApp/src/constants/Images.ts
+++ demoApp/src/constants/Images.ts
@@ -1,6 +1,6 @@
 export const Images = {
-   logo:require('../assets/logo.png'),
-   backArrow:require('../assets/backArrow.png'),
-   greenTick:require('../assets/greenTick.png'),
-   logoWithoutPadding:require('../assets/logoWithoutPadding.png')
-  };
+  logo: require('../assets/logo.png'),
+  backArrow: require('../assets/backArrow.png'),
+  greenTick: require('../assets/greenTick.png'),
+  logoWithoutPadding: require('../assets/logoWithoutPadding.png'),
+};

92c36396faf36d9b112a590bd44772ca18f42406
samonesim <sam.onesim@gmail.com>
2024-06-17 16:48:48 +0530

    fix: prepare lib

---
+++ lib/typescript/src/WrapperModel.d.ts
@@ -0,0 +1,11 @@
+export interface TotpResponse {
+    totp: string;
+    getRemainingSecs: number;
+}
+export declare enum DocType {
+    none = 0,
+    nationalId = 1,
+    drivingLicence = 2,
+    passport = 3
+}
+//# sourceMappingURL=WrapperModel.d.ts.map
